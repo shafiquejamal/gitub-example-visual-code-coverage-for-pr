b14c7f7a419091af604e29f6f55b5ee9185cab72
shafiquejamal <shafique.jamal@gmail.com>
2025-03-15 13:00:02 -0400

    Make changes to show code coverage

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ console.log(`The constant is:${CONSTANT}`);
 
 // See https://github.com/shafiquejamal/github-jest-coverage
 export const exampleForTestCoverage = (n: number): string => {
-	if (n > 5) {
-		return `Input number ${n} was greater than 5`;
+	if (n > 500) {
+		return `Input number ${n} was greater than 500.`;
 	}
 
-	return `Input number ${n} was not greater than 5`;
+	return `Input number ${n} was not greater than 500.`;
 };
